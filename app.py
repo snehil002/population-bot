27054d7dfe225e23e7950742a7d78dba7d6e9746
Snehil <snehil@example.com>
2023-10-19 14:39:13 +0530

    tab, pandas, logs

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import openai
 import gradio as gr
 import time
 import langchain
+import pandas as pd
 # from langchain.chains import RetrievalQA
 # from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import CSVLoader
@@ -13,7 +14,6 @@ from langchain.vectorstores import DocArrayInMemorySearch
 # from langchain.retrievers.document_compressors import EmbeddingsFilter
 # from langchain.retrievers import ContextualCompressionRetriever
 
-openai.api_key = os.environ['OPENAI_API_KEY']
 
 def get_completion(messages, model="gpt-3.5-turbo", temperature=0):
     response = openai.ChatCompletion.create(
@@ -22,6 +22,7 @@ def get_completion(messages, model="gpt-3.5-turbo", temperature=0):
         temperature=temperature
     )
     return response.choices[0].message["content"] #, response.usage
+
 
 def read_string_to_list(input_string):
     if input_string is None:
@@ -35,17 +36,29 @@ def read_string_to_list(input_string):
         print("Error: Invalid JSON string")
         return None
 
-file_name = "population_mod.csv"
 
-loader = CSVLoader(file_path=file_name)
-docs = loader.load()
+def initial_setup():
+    openai.api_key = os.environ['OPENAI_API_KEY']
 
-embeddings = OpenAIEmbeddings()
+    file_name = "population_mod.csv"
 
-db = DocArrayInMemorySearch.from_documents(
-    docs,
-    embeddings
-)
+    loader = CSVLoader(file_path=file_name)
+    docs = loader.load()
+
+    embeddings = OpenAIEmbeddings()
+
+    db = DocArrayInMemorySearch.from_documents(
+        docs,
+        embeddings
+    )
+
+    pop_df = pd.read_csv(file_name)
+
+    return db, pop_df
+
+
+db, pop_df = initial_setup()
+
 
 def create_chat_str(chat_hist_ui):
     chat_str = ""
@@ -54,6 +67,7 @@ def create_chat_str(chat_hist_ui):
         chat_str += f"Human: {usermsg}\n"
         chat_str += f"Assistant: {assmsg}\n"
     return chat_str
+
 
 # Create a standalone question
 def create_standalone_question(user_input, chat_history):
@@ -75,6 +89,7 @@ Standalone question:
     answer = get_completion(messages)
     print("standalone: ", answer)
     return answer
+
 
 def extract_country_and_columns(user_input):
     delimiter = "####"
@@ -105,9 +120,11 @@ rank
     response = get_completion(messages)
     return response
 
+
 def search_vectordb_by_country(query):
     res = db.max_marginal_relevance_search(query, k=1)
     return res[0].page_content
+
 
 def get_relevant_info(res):
     countries = res["countries"]
@@ -125,6 +142,7 @@ def get_relevant_info(res):
         info_bl += ["\n".join(info_l)]
     info = "\n\n".join(info_bl)
     return info
+
 
 def get_final_answer(user_input, rel_info):
     delimiter = "####"
@@ -146,8 +164,11 @@ User query will be delimited by {delimiter} characters."""
     # call_history += [response[1]]
     return response
 
+
 def process_input(user_input, context):
-    if user_input.strip() == "":
+    user_input = user_input.strip()
+    logs = {}
+    if user_input == "":
         raise Exception("No input")
 
 #     tokens = int(tokens)
@@ -155,51 +176,75 @@ def process_input(user_input, context):
         chat_history = create_chat_str(context)
         time.sleep(10)
         user_input = create_standalone_question(user_input, chat_history) # LLM
+        logs["standalone_question"] = user_input
 #         print(response_tup)
 #         user_input = response_tup[0]
 #         tokens += response_tup[1]["total_tokens"]
     
     time.sleep(10)
     response_str = extract_country_and_columns(user_input) # LLM
+    logs["country_attr_in_query"] = response_str
 #     print(response_tup)
 #     tokens += response_tup[1]["total_tokens"]
     response = read_string_to_list(response_str)
     rel_info = get_relevant_info(response)
+    logs["country_info_from_db"] = rel_info
 #     print(rel_info)
     
     time.sleep(10)
     answer = get_final_answer(user_input, rel_info) # LLM
+    logs["final_answer"] = answer
 #     print(response_tup)
 #     tokens += response_tup[1]["total_tokens"]
-    return answer
+    return answer, logs
 #     except Exception as exp:
 #         return exp.args
+
 
 def ui_func(user, history):
     return "", history + [[user, None]]
 
-def ui_func_2(history):
+
+def ui_func_2(history, logs_state):
     user = history.pop()[0]
+    
     try:
-        response = process_input(user, history)
-        history.append([user, response])
-        return "", history
+        response, curr_log = process_input(user, history)
+        history += [[user, response]]
+        user = ""
+        logs_state += [curr_log]
+        
     except Exception as exp:
         print("Error!")
-        print(exp.args)
-        gr.Warning(str(exp.args))
-        return user, history
+        print(exp)
+        gr.Warning(str(exp))
+        
+    return user, history, logs_state, logs_state
 
 with gr.Blocks() as demo:
-    gr.Markdown("""# Chat with Population Data 2022
+    gr.Markdown("""# Chat with World Population Data 2022
 Ask any question in the input field. Press Enter to Send. 😇 History remains on this page!""")
     
-    chatbot = gr.Chatbot(label="Chat History", height=400)
-    msg = gr.Textbox(label="User Input", placeholder="Enter your question")
-    clear = gr.ClearButton([msg, chatbot])
+    logs_state = gr.State([])
+
+    with gr.Tab("Chat Lounge"):
+        chatbot = gr.Chatbot(label="Chat History", height=400)
+        msg = gr.Textbox(label="User Input", placeholder="Enter your question")
+        sendbtn = gr.Button(value="Ask AI", variant="primary")
+        clear = gr.ClearButton([msg, chatbot])
+    
+    with gr.Tab("AI Chronicles"):
+        log_json_comp = gr.JSON([])
+
+    with gr.Tab("Population Data"):
+        gr.Dataframe(value=pop_df)
+
+    sendbtn.click(ui_func, [msg, chatbot], [msg, chatbot], queue=False).then(
+      ui_func_2, [chatbot, logs_state], [msg, chatbot, logs_state, log_json_comp]
+    )
     
     msg.submit(ui_func, [msg, chatbot], [msg, chatbot], queue=False).then(
-      ui_func_2, chatbot, [msg, chatbot]
+      ui_func_2, [chatbot, logs_state], [msg, chatbot, logs_state, log_json_comp]
     )
 
 demo.queue()
